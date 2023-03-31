6f1c978201acd30b33582c54f6833dbe778eb7bd
drdtdttt <102718973+drdtdttt@users.noreply.github.com>
2023-03-31 19:12:18 +0530

    Add files via upload

---
+++ detect_face_with_mask.py
@@ -0,0 +1,32 @@
+# import the opencv library
+import cv2
+import numpy as np
+import tensorflow as tf
+model=tf.keras.models.load_model('keras_model.h5',compile=False)
+
+  
+# define a video capture object
+vid = cv2.VideoCapture(0)
+  
+while(True):
+      
+    # Capture the video frame by frame
+    ret, frame = vid.read()
+    prediction=model.predict(frame)
+    print("Prediction : ",prediction)
+
+  
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+      
+    # Quit window with spacebar
+    key = cv2.waitKey(1)
+    
+    if key == 32:
+        break
+  
+# After the loop release the cap object
+vid.release()
+
+# Destroy all the windows
+cv2.destroyAllWindows()
